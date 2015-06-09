b37e6f928a497b040089972a3e88454bef418466
HAMANO Tsukasa <code@cuspy.org>
2015-06-09 17:43:50 +0900

    update 001.exs

--- elixir/001.exs
+++ elixir/001.exs
@@ -1,6 +1,8 @@
 #!/usr/bin/env elixir
 
+# https://projecteuler.net/problem=1
+
 1..1000
-|> Stream.filter(&(rem(&1, 3) == 0 or rem(&1, 5) == 0))
+|> Enum.filter(&(rem(&1, 3) == 0 or rem(&1, 5) == 0))
 |> Enum.sum
 |> IO.inspect
